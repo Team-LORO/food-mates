487649d381777541125ca0ac41b62c98a100907f
Mochammad Ainul Yaqin <mochammadainulyaqin@NB-MochammadAinulY-Tech.local>
2024-01-12 21:15:16 +0700

    initial commit

---
+++ app/src/main/java/com/foodmate/feature/auth/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.foodmate.feature.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foodmate.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
