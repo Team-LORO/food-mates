25f4bcad75f7cf63aafd836f14898b89b8dec0eb
Ainul Yaqin <ainul.y9b@gmail.com>
2024-01-18 20:54:13 +0700

    add: extract style from activity_login.xml

--- app/src/main/java/com/foodmate/feature/auth/LoginActivity.kt
+++ app/src/main/java/com/foodmate/feature/auth/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.foodmate.feature.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.foodmate.R
+import androidx.appcompat.app.AppCompatActivity
+import com.foodmate.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ctaRegister.setOnClickListener {
+            startActivity(
+                Intent(this@LoginActivity, RegistrationActivity::class.java)
+            )
+        }
     }
 }
